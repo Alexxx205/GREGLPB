67cf768cf370f3d579f7e46e759fda3eb9293259
Alex205 <alex.205@gmx.fr>
2017-11-30 12:04:18 +0100

    espace

--- formTest/Form1.cs
+++ formTest/Form1.cs
@@ -120,7 +120,7 @@ namespace formTest
                     ent.verifEmail();
                     ent.verifCode();
 
-                    //Insertion dans la base de données
+                    //Insertion dans la base de données 
                     //string req = "INSERT INTO res_partner(ref, name, street, state_id, street2, phone, fax, email, type, vat) VALUES ('"+ent.GetCode()+"','"+ent.GetRaison() + "','"+ent.GetAdresse() + "','" + ent.GetCP() + "','" + ent.GetVille() + "','" + ent.GetTel() + "','" + ent.GetFax() + "','" + ent.GetEmail() + "','" + ent.GetActif() + "','" + ent.GetReglement() +"');";
                     string req = "INSERT INTO res_partner(ref, name, street, state_id, street2, phone, fax, email, type, vat) VALUES ('ABC', 'testEnt', 'adr', '38200', 'Vienne', '0203040506', '2345678901', 'mail@mail.com', 'oui', 'cheque');";
                     dbcmd.CommandText = req;
